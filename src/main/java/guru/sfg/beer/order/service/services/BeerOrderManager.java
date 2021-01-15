310c84ed779844f9d5408d7a34fa65223ecb4559
luorenjie <lrj193927@gmail.com>
2021-01-14 20:47:25 -0500

    manage beer order via state machine

---
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,10 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+}
